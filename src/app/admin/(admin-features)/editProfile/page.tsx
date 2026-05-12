79b7d90c75ea2a9f0da3696b430cc954aa02b1b4
ShamsAziz03 <shamsimad550@gmail.com>
2026-05-12 23:16:24 +0300

    feat: add basic structure for remaining pages

---
+++ src/app/admin/(admin-features)/editProfile/page.tsx
@@ -0,0 +1,20 @@
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+
+const EditProfilePage = () => {
+	return (
+		<ScrollArea className="w-[100%]">
+			<div className="p-16">
+				<div className="mb-8">
+					<h1 className="text-3xl font-bold text-gray-800">Dashboard</h1>
+					<p className="text-gray-600 mt-1">
+						Welcome back! Here's what's happening today.
+					</p>
+				</div>
+			</div>
+
+			<ScrollBar orientation="vertical" />
+		</ScrollArea>
+	);
+};
+
+export default EditProfilePage;
