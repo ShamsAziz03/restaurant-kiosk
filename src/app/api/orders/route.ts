ff7c16fc49bec0d34c1cdc271baa93e1a4c84c46
ShamsAziz03 <shamsimad550@gmail.com>
2026-05-19 15:22:37 +0300

    feat: add features for kitchen staff -he can edit his profile -he can change the status of orders, and see all orders

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -33,3 +33,22 @@ export async function GET() {
 		headers: { "Content-Type": "application/json" },
 	});
 }
+
+export async function PUT(request: Request) {
+	const body = await request.json();
+	const result = await ordersService.updateOrderStatus(
+		body.orderId,
+		body.orderStatus,
+	);
+	if (result.success) {
+		return new Response(JSON.stringify({ success: true, msg: result.msg }), {
+			status: 201,
+			headers: { "Content-Type": "application/json" },
+		});
+	} else {
+		return new Response(JSON.stringify({ success: false, msg: result.msg }), {
+			status: 500,
+			headers: { "Content-Type": "application/json" },
+		});
+	}
+}
---
+++ src/app/kitchen-staff-features/layout.tsx
@@ -0,0 +1,75 @@
+"use client";
+import "../globals.css";
+import { ChefHat, LogOut, Pencil, ShoppingBag } from "lucide-react";
+import type { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { useLoggedUserStore } from "@/source/loggedUserStore";
+
+const navigation = [
+	{ name: "Orders", href: "/kitchen-staff-features/orders", icon: ShoppingBag },
+	{
+		name: "Edit Profile",
+		href: "/kitchen-staff-features/editProfile",
+		icon: Pencil,
+	},
+];
+
+function handleLogOut(router: AppRouterInstance, removeUser: () => void) {
+	removeUser();
+	router.replace("/admin");
+}
+
+export default function AdminLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	const pathname = usePathname();
+	const router = useRouter();
+	const removeUser = useLoggedUserStore((state) => state.removeLoggedUser);
+
+	return (
+		<section className="flex flex-row w-[100%] h-[100vh] relative">
+			{/* Sidebar */}
+			<div className="w-[20%] bg-white h-[100vh] fixed">
+				<div className="flex items-center gap-3 p-6 border-b">
+					<ChefHat className="w-8 h-8 text-gray-500" />
+					<h1 className="text-xl font-bold text-gray-800">Restaurant Admin</h1>
+				</div>
+				<nav className="p-4 space-y-2">
+					{navigation.map((item) => {
+						return (
+							<Link
+								className={`flex items-center gap-3 p-3 rounded-lg border-l-4
+    ${
+			pathname === item.href
+				? "border-indigo-500 bg-indigo-50 text-indigo-700"
+				: "text-gray-500 border-transparent"
+		}`}
+								href={item.href}
+								key={item.name}
+							>
+								<item.icon className="w-5 h-5" />
+								<span>{item.name}</span>
+							</Link>
+						);
+					})}
+				</nav>
+				<div className="flex items-center justify-center w-[90%] p-3 mt-8">
+					<Button
+						className="w-[70%] rounded-lg border-l-4"
+						onClick={() => handleLogOut(router, removeUser)}
+						type="button"
+					>
+						<LogOut className="w-5 h-5" />
+						<span>Log Out</span>
+					</Button>
+				</div>
+			</div>
+
+			<div className="flex-1 ml-[20%]">{children}</div>
+		</section>
+	);
+}
